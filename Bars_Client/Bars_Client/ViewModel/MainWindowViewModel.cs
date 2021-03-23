5e5af3758b75be0d897c0b8ce05a13c1060af38f
OnlyM1ss <sojiguy@mail.ru>
2021-03-23 17:48:55 +0400

    added structure
    added simple MVVM structure

---
+++ Bars_Client/Bars_Client/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using Bars_Client.Model;
+using Prism.Mvvm;
+
+namespace Bars_Client.ModelView
+{
+    class MainWindowViewModel : BindableBase
+    {
+        private Contract contractModel { get; }
+
+        public MainWindowViewModel()
+        {
+            contractModel = new Contract();
+            contracts = contractModel.GetContracts();
+        }
+
+        public List<Contract> Contracts
+        {
+            get => contracts;
+            set
+            {
+                contracts = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<Contract> contracts { get; set; }
+    }
+}
